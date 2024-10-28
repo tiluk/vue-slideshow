2e4fb02ac768abd3a9d4083913bd27185da126b4
tiluk <timlu.klement@gmail.com>
2024-10-28 22:04:13 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <Slideshow style="height: 100dvh" />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+//
+</script>
+
+<style lang="scss">
+body .v-main .v-app {
+  height: 100dvh;
+  margin: 0;
+}
+</style>
---
+++ src/components/Slideshow.vue
@@ -0,0 +1,145 @@
+<script setup lang="ts">
+import { ref, onMounted, nextTick, watch } from 'vue';
+
+type Slide = {
+    src: string;
+    type: "image" | "video";
+};
+
+const slides = ref<Slide[]>([]);
+const activeSlide = ref(0);
+const cycle = ref(true);
+const skipCycle = ref(false);
+const video = ref<HTMLVideoElement | null>(null);
+
+function shuffleArray(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+onMounted(() => {
+    const files = import.meta.glob('/public/slideshow/*.*');
+
+    slides.value = Object.keys(files).map(file => {
+        const fileType = file.endsWith('.mp4') ? 'video' : 'image';
+        const url = new URL(file, import.meta.url).href;
+        return {
+            src: url,
+            type: fileType
+        };
+    });
+
+    shuffleArray(slides.value);
+
+    setInterval(() => {
+        if (skipCycle.value) {
+            skipCycle.value = false;
+            return;
+        }
+        if (cycle.value) {
+            activeSlide.value = (activeSlide.value + 1) % slides.value.length;
+        }
+    }, 5000);
+});
+
+watch(activeSlide, () => {
+    if (slides.value[activeSlide.value].type === "video") {
+        console.log('video');
+        cycle.value = false;
+        nextTick(() => {
+            video.value = document.querySelector('video') as HTMLVideoElement;
+            video.value.currentTime = 0;
+            video.value.play();
+            video.value.onended = () => {
+                setTimeout(() => {
+                    activeSlide.value = (activeSlide.value + 1) % slides.value.length;
+                    skipCycle.value = true;
+                    cycle.value = true;
+                }, 2500);
+
+            };
+        });
+    } else {
+        if (video.value) {
+            video.value.pause();
+        }
+        cycle.value = true;
+    }
+
+    if (activeSlide.value === slides.value.length - 1) {
+        shuffleArray(slides.value);
+    }
+});
+
+</script>
+
+<template>
+    <v-carousel v-model="activeSlide" :touch="{
+        left: () => { activeSlide++; skipCycle = true; },
+        right: () => { activeSlide--; skipCycle = true; }
+    }" hide-delimiters show-arrows="hover">
+
+        <template v-slot:prev="{ props }">
+            <v-btn class="custom-arrow left" icon="$prev" @click="() => { props.onClick(); skipCycle = true; }"></v-btn>
+        </template>
+
+        <template v-slot:next="{ props }">
+            <v-btn class="custom-arrow right" icon="$next"
+                @click="() => { props.onClick(); skipCycle = true; }"></v-btn>
+        </template>
+
+        <v-carousel-item v-for="(slide, i) in slides" :key="i">
+            <v-sheet class="fit-container">
+                <v-img :src="slide.src" class="fit-image" v-if="slide.type === `image`" />
+                <video :src="slide.src" class="fit-image" controls autoplay v-else-if="slide.type === `video`" />
+            </v-sheet>
+        </v-carousel-item>
+    </v-carousel>
+</template>
+
+<style lang="scss" scoped>
+.v-carousel {
+    height: 100vh;
+}
+
+.v-carousel-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.fit-container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.fit-image {
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: contain;
+}
+
+.custom-arrow {
+    display: none;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+}
+
+.custom-arrow.left {
+    left: 10px;
+}
+
+.custom-arrow.right {
+    right: 10px;
+}
+
+.v-carousel:hover .custom-arrow {
+    display: block;
+}
+</style>
